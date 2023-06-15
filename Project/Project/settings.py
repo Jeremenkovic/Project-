6534ce83adc624f67272b57ab2c3eef18e72390c
Jeremenkovic <jeremenkovic23@gmail.com>
2023-06-15 16:50:13 +0200

    fixes

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,6 +123,9 @@ STATIC_URL = 'static/'
 # Celery settings
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- Project/api/views.py
+++ Project/api/views.py
@@ -3,14 +3,24 @@ from django.http import JsonResponse, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.decorators import api_view
 from .tasks import process_csv
+from django.conf import settings
+import os
 
 @api_view(['POST'])
 @csrf_exempt
 def schedule_file_processing(request):
-    file = request.FILES['file']
-    output_file = 'output_' + file.name
-    task = process_csv.delay(file.temporary_file_path(), output_file)
-    return JsonResponse({'task_id': str(task.id)})
+    if request.method == "POST":
+        file = request.FILES["file"]
+        output_file = os.path.join(settings.MEDIA_ROOT, f'output_{file.name}')
+        # Write uploaded file to disk
+        input_file_path = os.path.join(settings.MEDIA_ROOT, file.name)
+        with open(input_file_path, 'wb+') as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+        # Pass the path of the saved file to the task
+        task = process_csv.delay(input_file_path, output_file)
+        return JsonResponse({"task_id": str(task.id)}, status=202)
+    return JsonResponse({"detail": "Invalid method"}, status=400)
 
 
 @api_view(['GET'])
